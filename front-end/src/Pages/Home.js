3366a6ccac45e06cab048ec9015486186a8682b4
EmaleeSoto <emaleesoto@gmail.com>
2022-08-22 02:16:33 -0400

    Final push, css complete, only missing 2 CSS tests

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,9 +1,18 @@
 function Home() {
   return (
-    <div className="Home">
-      <h1>Welcome to the Snack-A-Log!</h1>
+    <div className="Home-Page">
+      <header className="Home">
+        <h1>Welcome to the Snack-A-Log!</h1>
+      </header>
     </div>
   );
 }
 
 export default Home;
+
+{
+  /* <img
+src="https://st2.depositphotos.com/1005573/6189/i/600/depositphotos_61892893-stock-photo-savoury-snacks.jpg"
+alt="snack-image"
+/> */
+}
